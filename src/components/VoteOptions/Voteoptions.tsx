ce59b7ff6652d01a4ce7eaf812a81f60e48b0a4e
unknown <dmytriy.litvin@gmail.com>
2025-12-16 15:11:07 +0200

    project was done

---
+++ src/components/VoteOptions/Voteoptions.tsx
@@ -0,0 +1,46 @@
+// import css from "./VoteOptions.module.css"
+
+// interface VoteOptionsProps {
+//     onVote,
+//     onReset,
+//     canReset: boolean
+// }
+
+// function VoteOptions () {
+//     return <>
+//     <div className={css.container}>
+//   <button className={css.button}>Good</button>
+//   <button className={css.button}>Neutral</button>
+//   <button className={css.button}>Bad</button>
+//   <button className={`${css.button} ${css.reset}`}>Reset</button>
+//     </div>
+// </>
+// }
+
+
+// export default VoteOptions
+
+
+import type { VoteType } from "../../types/vote";
+import css from "./VoteOptions.module.css";
+
+interface VoteOptionsProps {
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({ onVote, onReset, canReset }: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => onVote("good")}>Good</button>
+      <button className={css.button} onClick={() => onVote("neutral")}>Neutral</button>
+      <button className={css.button} onClick={() => onVote("bad")}>Bad</button>
+      {canReset && (
+        <button className={`${css.button} ${css.reset}`} onClick={onReset}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
